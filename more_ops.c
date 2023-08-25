718f572389039fbeabbb7d946a2cd451ab1226fe
Karen Miano <mianokaren1798@gmail.com>
2023-08-24 18:34:30 -0700

    Implementing the pop opcode

--- more_ops.c
+++ more_ops.c
@@ -3,8 +3,8 @@
 #include <stdio.h>
 /**
  * pop - pops the top element from a stack
- * stack: pointer to pointer to top element of stack
- * line_number: line_number where the opcode is called
+ * @stack: pointer to pointer to top element of stack
+ * @line_number: line_number where the opcode is called
  * Return: nothing
  */
 void pop(stack_t **stack, unsigned int line_number)
@@ -14,7 +14,7 @@ void pop(stack_t **stack, unsigned int line_number)
 	if (*stack == NULL)
 	{
 		fprintf(stderr, "L%u: can't pop an empty stack\n", line_number);
-		exit (EXIT_FAILURE);
+		exit(EXIT_FAILURE);
 	}
 	temp = *stack;
 	*stack = temp->next;
